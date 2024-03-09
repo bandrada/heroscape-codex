a06a8b4d0008e8f9d9eedae8b5b5d19049605ffe
bandrada.dev <bandrada.dev@gmail.com>
2024-03-09 14:22:46 -0600

    test back

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import * as data from './cards.json'
+//import * as data from './cards.json'
 
-const thing = JSON.parse(data);
+//const thing = JSON.parse(data);
 function testFunc() {
-    console.log(thing);
+    console.log('thing');
 }
